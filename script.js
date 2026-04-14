20f214dbc59fa95464e2f549bffc657e7f6e0e0f
Lupe2026 <lupestamp2023@gmail.com>
2026-04-14 13:30:25 -0300

    Add files via upload
    Este App está na sua primeira versão.

---
+++ script.js
@@ -0,0 +1,74 @@
+const categories = ["Alimentação", "Higiene e Limpeza", "Utensílios Domésticos", "Cama, Mesa e Banho", "Bazar"];
+let shoppingList = JSON.parse(localStorage.getItem('myList')) || [
+    { id: 1, name: 'Arroz', category: 'Alimentação' },
+    { id: 2, name: 'Sabonete', category: 'Higiene e Limpeza' }
+];
+
+function renderList() {
+    const container = document.getElementById('listsContainer');
+    container.innerHTML = '';
+
+    categories.forEach(cat => {
+        const filteredItems = shoppingList.filter(item => item.category === cat);
+        if (filteredItems.length > 0) {
+            const section = document.createElement('div');
+            section.className = 'category-section';
+            section.innerHTML = `<h3 class="category-title">${cat}</h3><ul id="list-${cat}"></ul>`;
+            container.appendChild(section);
+
+            const ul = section.querySelector('ul');
+            filteredItems.forEach(item => {
+                const li = document.createElement('li');
+                li.innerHTML = `
+                    <span>${item.name}</span>
+                    <div class="actions">
+                        <button class="btn-edit" onclick="editItem(${item.id})">Editar</button>
+                        <button class="btn-delete" onclick="deleteItem(${item.id})">Excluir</button>
+                    </div>
+                `;
+                ul.appendChild(li);
+            });
+        }
+    });
+    localStorage.setItem('myList', JSON.stringify(shoppingList));
+}
+
+function addItem() {
+    const name = document.getElementById('itemInput').value;
+    const category = document.getElementById('categoryInput').value;
+    if (!name) return;
+
+    shoppingList.push({ id: Date.now(), name, category });
+    document.getElementById('itemInput').value = '';
+    renderList();
+}
+
+function deleteItem(id) {
+    shoppingList = shoppingList.filter(item => item.id !== id);
+    renderList();
+}
+
+function editItem(id) {
+    const item = shoppingList.find(i => i.id === id);
+    const newName = prompt("Editar item:", item.name);
+    if (newName) {
+        item.name = newName;
+        renderList();
+    }
+}
+
+renderList();
+
+window.addEventListener('load', () => {
+    const splash = document.getElementById('splash-screen');
+    
+    // Define o tempo de 4000 milissegundos (4 segundos)
+    setTimeout(() => {
+        splash.classList.add('hidden');
+        
+        // Remove do DOM após a transição para não atrapalhar o toque na tela
+        setTimeout(() => {
+            splash.style.display = 'none';
+        }, 500); 
+    }, 4000);
+});
